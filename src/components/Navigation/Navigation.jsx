72e5204d8aa710737f0bf0706ac3102a89542e46
Svitlana Riabtseva <161156007+Svitlana-Riabtseva@users.noreply.github.com>
2024-11-20 18:18:46 +0200

    add useSelector in component navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,21 +1,29 @@
 import clsx from 'clsx';
 import css from './Navigation.module.css';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Navigation = () => {
+  // Отримання стану isLoggedIn з Redux
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+
+  // Функція для створення динамічних класів
   const buildCssClasses = ({ isActive }) =>
     clsx(css.link, isActive && css.active);
+
   return (
-    <div>
+    <nav>
       <NavLink className={buildCssClasses} to="/">
         Home
       </NavLink>
-      <NavLink className={buildCssClasses} to="/contacts">
-        Contacts
-      </NavLink>
-    </div>
+      {/* Умовний рендеринг посилання на /contacts */}
+      {isLoggedIn && (
+        <NavLink className={buildCssClasses} to="/contacts">
+          Contacts
+        </NavLink>
+      )}
+    </nav>
   );
 };
 
 export default Navigation;
-
